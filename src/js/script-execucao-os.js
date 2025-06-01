af5b0f2ca23ddb649a9afe02bbb2570587560434
kalito.silva <kalito.silva@santacolomba.com.br>
2025-06-01 08:44:44 -0300

    modificacoes e atualizacoes do projeto 07

---
+++ src/js/script-execucao-os.js
@@ -0,0 +1,6 @@
+let percTeste = 60;
+let lamTeste = 10.25;
+
+lam100 = (lamTeste * percTeste) / 100;
+
+console.log(lam100);
---
+++ src/js/script-lamina.js
@@ -0,0 +1,13 @@
+const percTeste = document.getElementById('percTeste');
+const laminaTeste = document.getElementById('laminaTeste');
+const laminaInicial = document.getElementById('laminaInicial');
+
+function calcularLamina() {
+
+    const porcentagem = parseFloat(percTeste.value) || 0;
+    const resultadoTeste = parseFloat(laminaTeste.value) || 0;
+
+    laminaInicial.value = (resultadoTeste * porcentagem) / 100; 
+}
+
+laminaTeste.addEventListener('input', calcularLamina);
